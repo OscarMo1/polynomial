fbbd64971d2320dbdce72649d285a620725f42d7
oscar mo <oscarmo@bu.edu>
2024-09-11 17:25:03 -0400

    cs 506 exercise part b

--- polynomial.py
+++ polynomial.py
@@ -5,12 +5,18 @@ class X:
     def __repr__(self):
         return "X"
 
+    def evaluate(self, x_value):
+        return x_value
+
 class Int:
     def __init__(self, i):
         self.i = i
     
     def __repr__(self):
         return str(self.i)
+
+    def evaluate(self, x_value):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -20,6 +26,9 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
 
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) + self.p2.evaluate(x_value)
+
 class Mul:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -28,12 +37,21 @@ class Mul:
     def __repr__(self):
         if isinstance(self.p1, Add):
             if isinstance(self.p2, Add):
-                 return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
+                return "( " + repr(self.p1) + " ) * ( " + repr(self.p2) + " )"
             return "( " + repr(self.p1) + " ) * " + repr(self.p2)
         if isinstance(self.p2, Add):
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
 
+    def evaluate(self, x_value):
+        return self.p1.evaluate(x_value) * self.p2.evaluate(x_value)
 
-poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+# Define the polynomial
+poly = Add(Add(Int(4), Int(3)), Add(X(), Mul(Int(1), Add(Mul(X(), X()), Int(1)))))
+
+# Print the polynomial structure
 print(poly)
+
+# Evaluate the polynomial for X = -1
+result = poly.evaluate(-1)
+print(result)
